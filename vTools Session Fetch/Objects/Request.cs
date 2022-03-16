a6d10a5333288466852ec0fc310993d7bc5886dd
Dixie J <devildjoseph666@gmail.com>
2022-03-16 10:31:04 +0800

    Added Request object

---
+++ vTools Session Fetch/Objects/Request.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vTools_Session_Fetch.Objects
+{
+    public partial class Headers
+    {
+        public string Host { get; set; } = String.Empty;
+        public string Connection { get; set; } = String.Empty;
+        public string UserAgent { get; set; } = String.Empty;
+        public Dictionary<string, string> Others { get; set; } = new Dictionary<string,string>();
+    }
+    public class Request
+    {
+        public string raw { get; set; } = String.Empty;
+        public string Route { get; set; } = String.Empty;
+        public string Method { get; set; } = String.Empty;
+        public Headers Headers { get; set; } = new Headers();
+
+        public Request(String incommingMessage)
+        {
+            this.raw = incommingMessage;
+            var message = incommingMessage.Replace("\r", String.Empty).Split('\n');
+            foreach (var item in message)
+            {
+                var line = item.Trim().ToLower().Split(':');
+                switch (line[0])
+                {
+                    case "host":
+                        Headers.Host = line[1].Trim();
+                        break;
+                    case "connection": 
+                        Headers.Connection = line[1].Trim();
+                        break;
+                    case "user-agent":
+                        Headers.UserAgent = line[1].TrimStart(' ');
+                        break;
+                    default:
+                        try
+                        {
+                            Headers.Others.Add(line[0], line[1]);
+                        } catch (IndexOutOfRangeException)
+                        {
+                            var route = item.Split(" HTTP/")[0].Split(' ');
+                            if (route.Length < 2) break;
+                            Route = route[1];
+                            Method = route[0];
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
--- vTools Session Fetch/Server/HTTPServer.cs
+++ vTools Session Fetch/Server/HTTPServer.cs
@@ -8,15 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using vTools_Session_Fetch.Server.Interface;
+using vTools_Session_Fetch.Objects;
 
 namespace vTools_Session_Fetch.Server
 {
     public class HttpServer : IHttpServer
     {
         private readonly TcpListener listener;
-
+        private int _port;
         public HttpServer(int port)
         {
+            _port = port;
             this.listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
         }
 
@@ -29,7 +31,8 @@ namespace vTools_Session_Fetch.Server
                 var buffer = new byte[10240];
                 var stream = client.GetStream();
                 var length = stream.Read(buffer, 0, buffer.Length);
-                var incomingMessage = Encoding.UTF8.GetString(buffer, 0, length);
+                var incomingMessage = new Request(Encoding.UTF8.GetString(buffer, 0, length));
+                if (!incomingMessage.Headers.UserAgent.Contains($"port/{_port}")) continue;
                 var result = $"{{\"cookies\":\"{cookies}\"}}";
                 var response = "HTTP/1.0 200 OK" + Environment.NewLine
                         + "Content-Length: " + result.Length + Environment.NewLine
@@ -39,6 +42,8 @@ namespace vTools_Session_Fetch.Server
                         + result
                         + Environment.NewLine + Environment.NewLine;
                 stream.Write(Encoding.UTF8.GetBytes(response));
+                Logger.InfoLogger.Log(message: incomingMessage.raw);
+                Logger.InfoLogger.Log(message: response);
             }
         }
     }
